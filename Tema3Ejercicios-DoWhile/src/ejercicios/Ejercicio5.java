d1d35aa0e87968d9278813f5ec5f135e73d37811
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2022-11-08 18:41:55 +0100

    ej 5 comen

--- Tema3Ejercicios-DoWhile/src/ejercicios/Ejercicio5.java
+++ Tema3Ejercicios-DoWhile/src/ejercicios/Ejercicio5.java
@@ -6,31 +6,53 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		
-		int numero;
+		int numero; //Declaro variable en la que guardare el numero con el que el programa
+		//intentara adivinar el numero en el que ha pensado el usuario
 		
-		String respuesta="";
+		String respuesta=""; //Declaro variable tipo String, la inicializo vacia y guardo en ella
+		//la pista que el usuario da al programa con MAYOR O MENOR
 		
+		int menor=0;//Declaro variable, la inicializo a 0 para poder usarla en el bucle
+		// y guardo en ella el numero mas bajo que quiero permitir usar al programa 
+		//para adivinar el numero en el que el usuario ha pensado
+
+		int mayor=101;//Declaro variable, la inicializo a 101 para poder usarla en el bucle
+		// y guardo en ella el numero mas alto que quiero permitir usar al programa 
+		//para adivinar el numero en el que el usuario ha pensado
+		
+		//Creo Scanner, lo llamo dogma y lo importo
 		Scanner dogma = new Scanner(System.in);
 		
-		int menor=0;
-		
-		int mayor=101;
-		
+		//Creo el bucle
 		do {
+			//Creo numero aleatorio y lo asigno a variable numero
 			numero = (int)(Math.random()*(mayor-menor)+menor);
-			System.out.println("¿Es el número " + numero + "?");
-			respuesta = dogma.nextLine();
 			
+			//Imprimo valor de variable numero por pantalla
+			System.out.println("¿Es el número " + numero + "?");
+			
+			//Recojo un valor proporcionado por el usuario para variable String respuesta usando el Scanner 
+			
+			//Si variable respuesta es igual a MAYOR el numero anterior usado por el programa en
+			//el print pasara a ser el numero mas alto permitido en el siguiente print en la siguiente iteracion del bucle
 			if(respuesta.equals("MAYOR")) {
 				mayor = numero;
+				
+			//Si variable respuesta es igual a MENOR el numero anterior usado por el programa en
+			//el print pasara a ser el numero mas bajo permitido en el siguiente print en la siguiente iteracion del bucle
 			} else if(respuesta.equals("MENOR")) {
 				menor = numero;
 			}
 			
+			//Una vez el programa me diga cual es el numero en el que estaba pensando se lo indico 
+			//escribiendo IGUAL cuando me solicite la pista,
+			//lo cual hara que deje de cumplirse la condicion que mantiene el bucle en ejecucion
 		}while(!respuesta.equals("IGUAL"));
 		
+		//Imprimo mensaje por pantalla notificando el exito del programa
 		System.out.println("¡¡ENHORABUENA!! Has acertado");
 
+		//Cierro Scanner
 		dogma.close();
 		
 	}
